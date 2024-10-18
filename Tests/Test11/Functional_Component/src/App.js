9ea1212996cbbc5d5ba8e79ea3e0fe7a6dd952f8
ppunem <prathyusha20994@gmail.com>
2024-10-18 18:14:27 +0530

    added more tasks and assignments

---
+++ Tests/Test11/Functional_Component/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+      return (
+          <div className='bg'>
+            <h1 className='main-head'>Congratulations</h1>
+            <div className='card'>
+             <h1 className='name'>Prathyusha Punem</h1>
+             <p className='college'>
+               Aurora's Technological and Research Institute
+             </p>
+          </div>
+      </div>
+      );
+     }
+ 
+     
+export default App;
